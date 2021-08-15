48da310741ae0163308e1435b4625227078ceacb
Julio <beito_xp@me.com>
2021-08-15 13:01:54 -0600

    Enviroment Variable Enable

--- src/components/EventDesing.js
+++ src/components/EventDesing.js
@@ -7,19 +7,13 @@ import { EventTemplate } from './EventTemplate';
 export const EventDesing = () => {//"Jodc23gOew"
 
   const { eventId } = useParams();
-  //
-  // const hero = useMemo(() => getHeroById( heroeId ), [ heroeId ]);
-  //
-  // if ( !hero ) {
-  //     return <Redirect to="/" />;
-  // }
 
-    const { data: baseinfo, loading } = useFetchEvent(eventId);
-    console.log(baseinfo);
-    console.log(loading);
-    return (
-      <main>
-        {!loading && <EventTemplate eventName={baseinfo.name} hostName={baseinfo.createdBy.name} eventDescription={baseinfo.description} imageUrl={baseinfo.banner.url} imageName={baseinfo.banner.name} />}
-      </main>
-    )
+  const { data: baseinfo, loading } = useFetchEvent(eventId);
+
+  return (
+    <main>
+      {!loading && <EventTemplate eventName={baseinfo.name} hostName={baseinfo.createdBy.name} eventDescription={baseinfo.description} imageUrl={baseinfo.banner.url} imageName={baseinfo.banner.name} />}
+    </main>
+  )
+
 }
--- src/helpers/getEvent.js
+++ src/helpers/getEvent.js
@@ -1,10 +1,16 @@
+const { REACT_APP_REST_API_KEY, REACT_APP_API_KEY } = process.env;
+
 export const getEvent = async(eventID) => {
-    const url = `https://parseapi.back4app.com/classes/Event/${eventID}`;
-    const resp = await fetch(url, {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache'
-    });
-    const eventResponse = await resp.json();
-    return eventResponse;
+  const url = `https://parseapi.back4app.com/classes/Event/${eventID}`;
+  const resp = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      "X-Parse-Application-Id": REACT_APP_API_KEY,
+      "X-Parse-REST-API-Key": REACT_APP_REST_API_KEY
+    }
+  });
+  const eventResponse = await resp.json();
+  return eventResponse;
 }
