eab8dbd26462ae21db3f89015332d0d4e319bc2a
Julio <beito_xp@me.com>
2021-08-20 17:40:44 -0600

    Responsive Events 2

--- src/components/EventTemplate.js
+++ src/components/EventTemplate.js
@@ -5,19 +5,41 @@ import AppStore from "../images/appstore.png";
 export const EventTemplate = ({eventName = "", hostName = "", eventDescription = "", imageUrl = "", imageName = ""}) => {
 
   return (
-    <section className="sectionDisplay">
-      <div className="imageDisplay">
+    <>
+      <section className="float">
         <img src={imageUrl} alt={imageName} />
-      </div>
-      <p className="hostName">Hosted by: {hostName}</p>
-      <p className="eventName">{eventName}</p>
-      <p className="contextName">{eventDescription}</p>
-      <p className="joinName"><b>Download to join →</b></p>
-      <img src={AppStore} alt="AppStore" className="finalBottons" />
-      <img src={PlayStore} alt="PlayStore" className="finalBottons" />
-    </section>
+      </section>
+      <article>
+        <p className="hostName">Hosted by: {hostName}</p>
+        <h2 className="eventName">{eventName}</h2>
+        <p>
+          Letsecho built the Letsecho app as
+          an Ad Supported app. This SERVICE is provided by
+          Letsecho at no cost and is intended for use as
+          is.
+        </p>
+        <p className="joinName">
+          <b>Download to join →</b>
+        </p>
+        <img src={AppStore} alt="AppStore" className="finalBottons" />
+        <img src={PlayStore} alt="PlayStore" className="finalBottons" />
+      </article>
+      <div className="clearfixed"></div>
+    </>
   )
 }
+
+// <section className="sectionDisplay">
+//   <div className="imageDisplay">
+//     <img src={imageUrl} alt={imageName} />
+//   </div>
+//   <p className="hostName">Hosted by: {hostName}</p>
+//   <p className="eventName">{eventName}</p>
+//   <p className="contextName">{eventDescription}</p>
+//   <p className="joinName"><b>Download to join →</b></p>
+//   <img src={AppStore} alt="AppStore" className="finalBottons" />
+//   <img src={PlayStore} alt="PlayStore" className="finalBottons" />
+// </section>
 
 /*
 <Container>
