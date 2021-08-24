b56aebb95b0fdc2408de93981dcd3874d826699a
Julio <beito_xp@me.com>
2021-08-24 09:15:14 -0600

    Minor Corrections

--- src/components/EventTemplate.js
+++ src/components/EventTemplate.js
@@ -5,23 +5,29 @@ import AppStore from "../images/appstore.png";
 export const EventTemplate = ({eventName = "", hostName = "", eventDescription = "", imageUrl = "", imageName = ""}) => {
 
   return (
-    <section className="sectionDisplay">
-      <section className="float">
-        <img src={imageUrl} alt={imageName} />
+    <>
+      <section className="sectionDisplay">
+        <section className="float">
+          <img src={imageUrl} alt={imageName} />
+        </section>
+        <article className="nonMarginArticle">
+          <p className="hostName">Hosted by: {hostName}</p>
+          <h2 className="eventName">{eventName}</h2>
+          <p>
+            {eventDescription}
+          </p>
+          <p className="joinName">
+            <b>Download to join →</b>
+          </p>
+          <a href="https://apps.apple.com/app/letsecho/id1563740904">
+            <img src={AppStore} alt="AppStore" className="finalBottons" />
+          </a>
+          <a href="https://play.google.com/store/apps/details?id=com.letsecho.letsechoapp&hl=es_419&gl=US">
+            <img src={PlayStore} alt="PlayStore" className="finalBottons" />
+          </a>
+        </article>
       </section>
-      <article className="nonMarginArticle">
-        <p className="hostName">Hosted by: {hostName}</p>
-        <h2 className="eventName">{eventName}</h2>
-        <p>
-          {eventDescription}
-        </p>
-        <p className="joinName">
-          <b>Download to join →</b>
-        </p>
-        <img src={AppStore} alt="AppStore" className="finalBottons" />
-        <img src={PlayStore} alt="PlayStore" className="finalBottons" />
-      </article>
       <div className="clearfixed"></div>
-    </section>
+    </>
   )
 }
