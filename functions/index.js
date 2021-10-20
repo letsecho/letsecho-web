ee1afb6e0db91bdd4135b19a6c660fa49f916c26
Julio <beito_xp@me.com>
2021-10-20 11:25:02 -0600

    Index Function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require("firebase-functions");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const fs = require("fs");
+exports.preRender = functions.https.onRequest((request, response) => {
+  const path = request.path ? request.path.split("/") : request.path;
+  let index = fs.readFileSync("./web/index.html").toString();
+  const setMetas = (title, description) => {
+    index = index.replace("INJECTED_TITLE", title);
+    index = index.replace("INJECTED_DESCRIPTION", description);
+  };
+  switch (path[1]) {
+    case "faq": {
+      setMetas("FAQ", "Frequently Asked Questions");
+    } break;
+    case "privacy": {
+      setMetas("Privacy", "Privacy Politics");
+    } break;
+    case "terms": {
+      setMetas("Terms", "Terms and Conditions");
+    } break;
+    default: {
+      setMetas("TestA", "Discover spontaneous activities");
+    }
+  }
+  response.status(200).send(index);
+});
