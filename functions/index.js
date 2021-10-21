6535bdde374a74bf7324bc884bb98006ec6928b5
Julio <beito_xp@me.com>
2021-10-20 22:38:35 -0600

    Firebase Function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 const fs = require("fs");
+const config = functions.config();
+const {rest, api} = config.slack;
 exports.preRender = functions.https.onRequest((request, response) => {
   const path = request.path ? request.path.split("/") : request.path;
   let index = fs.readFileSync("./web/index.html").toString();
@@ -17,9 +19,28 @@ exports.preRender = functions.https.onRequest((request, response) => {
     case "terms": {
       setMetas("Terms", "Terms and Conditions");
     } break;
+    case "events": {
+      let eventResponse = getEvent(path[2]);
+      setMetas(eventResponse.data.name, "Terms and Conditions");
+    } break;
     default: {
       setMetas("TestA", "Discover spontaneous activities");
     }
   }
   response.status(200).send(index);
 });
+
+const getEvent = async(pathName) => {
+  const url = `https://api.letsechoapp.com/classes/Event/${pathName}`;
+  const resp = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      "X-Parse-Application-Id": api,
+      "X-Parse-REST-API-Key": rest
+    }
+  });
+  let eventResponse = await resp.json();
+  return eventResponse;
+}
