4a59e58ba91eeb60e0320401e6916cbfbede207d
Julio <beito_xp@me.com>
2021-11-05 08:18:29 -0600

    Test on Event Design and Event Template

---
+++ src/tests/components/EventDesign.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import { shallow } from 'enzyme';
+import { EventDesign } from '../../components/EventDesign';
+
+
+describe('Test on <EventDesign />', () => {
+
+});
---
+++ src/tests/components/EventTemplate.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import { shallow } from 'enzyme';
+import { EventTemplate } from '../../components/EventTemplate';
+
+
+describe('Test on <EventTemplate />', () => {
+
+});
