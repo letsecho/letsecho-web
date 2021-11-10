767cb771b74216d31beec20b2ff3b71cf04d5979
Julio <beito_xp@me.com>
2021-11-10 03:24:10 -0600

    The Download Section is set aside.

--- src/components/NotFoundEvent.js
+++ src/components/NotFoundEvent.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import PlayStore from "../images/playstore.png";
-import AppStore from "../images/appstore.png";
 import NotFoundImage from "../images/Square.svg";
+import { DownloadSection } from './DownloadSection';
 
 export const NotFoundEvent = () => {
   return (
@@ -16,17 +15,7 @@ export const NotFoundEvent = () => {
           <p>
             The event is not available. Download the Letsecho app to find and create more gatherings.
           </p>
-          <div className="stickyPlacer">
-            <p className="joinName">
-              <b>Download to join <span className="bigArrow">➔</span></b>
-            </p>
-            <a href="https://apps.apple.com/app/letsecho/id1563740904">
-              <img src={AppStore} alt="AppStore" className="finalBottons" />
-            </a>
-            <a href="https://play.google.com/store/apps/details?id=com.letsecho.letsechoapp&hl=es_419&gl=US">
-              <img src={PlayStore} alt="PlayStore" className="finalBottons" />
-            </a>
-          </div>
+          <DownloadSection />
         </article>
       </section>
       <div className="clearfixed"></div>
