eeab3d29316247283f16fbc6d46bfa687daf1f3d
Julio <beito_xp@me.com>
2021-11-24 10:26:38 -0600

    Remove Spaces

--- src/components/EventDesign.js
+++ src/components/EventDesign.js
@@ -8,7 +8,6 @@ import { NotFoundEvent } from './NotFoundEvent';
 import { LoadingSection } from './LoadingSection';
 
 export const EventDesign = () => {
-
   const { eventId } = useParams();
   const { data: baseinfo, loading } = useFetchEvent(eventId);
 
@@ -21,5 +20,4 @@ export const EventDesign = () => {
       {(!loading) ? ((Object.entries(baseinfo).length > 0) ? <EventTemplate eventName={baseinfo.name} hostName={baseinfo.createdBy.name} eventDescription={baseinfo.description} imageUrl={baseinfo.banner?.url} imageName={baseinfo.banner?.name} /> : <NotFoundEvent/>) : (<LoadingSection/>)}
     </main>
   )
-
 }
