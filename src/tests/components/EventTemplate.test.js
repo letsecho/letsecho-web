0b0e4e9a6339c5499d13aa2af5bc1531abaa43f2
Julio <beito_xp@me.com>
2021-12-01 17:00:46 -0600

    Test 2 (Component Content)

--- src/tests/components/EventTemplate.test.js
+++ src/tests/components/EventTemplate.test.js
@@ -6,10 +6,22 @@ import { EventTemplate } from '../../components/EventTemplate';
 
 describe('Test on <EventTemplate />', () => {
 
-  test('Displaying an empty EventTemplate Component', () => {
-    const testName = "testA";
+  test('Testing eventName for EventTemplate Component', () => {
+    const testName = "testName";
     const { getByText } = render(<EventTemplate eventName={testName} />);
     expect(getByText(testName)).toBeInTheDocument();
+  });
+
+  test('Testing hostName for EventTemplate Component', () => {
+    const testHost = "testHost";
+    const { getByText } = render(<EventTemplate hostName={testHost} />);
+    expect(getByText(`Hosted by: ${testHost}`)).toBeInTheDocument();
+  });
+
+  test('Testing eventDescription for EventTemplate Component', () => {
+    const testDescription = "testDescription";
+    const { getByText } = render(<EventTemplate eventDescription={testDescription} />);
+    expect(getByText(testDescription)).toBeInTheDocument();
   });
 
 });
